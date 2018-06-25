35b95e93fe0d2450c3b7f4b0d6d006b6bbd76b7b
ingrid-anderson <ingrid@warriormedia.com>
2018-06-25 05:45:31 -0700

    Added youtube

--- wm/aws/conf.py
+++ wm/aws/conf.py
@@ -2,8 +2,10 @@ import datetime
 import os
 
 AWS_ACCESS_KEY_ID = "AKIAIZGDP6ARV5OO4N7A"  # "AKIAI4ELCXQMLV6QIE2A"
-AWS_SECRET_ACCESS_KEY = "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"  # "HVignQ85OVOZx6SNgGLWfZfc4VHeDsSix9UVvwgY"
+AWS_SECRET_ACCESS_KEY = "Z+YPsvd4CzPCR2J5xgDw06zhmGGrj73ojt7uRzh6"  # "HVignQ85OVOZx6SNgGLWfZfc4VHeDsSix9UVvwgY"
 #user name is django-user
+
+PREPEND_WWW = True
 
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
@@ -14,15 +16,15 @@ STATICFILES_STORAGE = 'wm.aws.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME =  'ka-bucket' # 'wm-dv-static-media'
 # AWS_UPLOAD_USERNAME = "wm-Justin"
 # AWS_UPLOAD_GROUP = "wm-dv-group"
-S3DIRECT_REGION = 'us-west-2'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+S3DIRECT_REGION = 's3-us-west-2'
+S3_URL = '//%s.amazonaws.com/%s/' % (S3DIRECT_REGION, AWS_STORAGE_BUCKET_NAME)
+# MEDIA_URL = '//%s.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 
-PROTECTED_DIR_NAME = 'media'
-PROTECTED_MEDIA_URL_SSS = '//%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, PROTECTED_DIR_NAME)
-PROTECTED_MEDIA_URL = '//%s.%s.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, S3DIRECT_REGION, PROTECTED_DIR_NAME)
+# PROTECTED_DIR_NAME = 'media'
+# PROTECTED_MEDIA_URL_SSS = '//%s.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, PROTECTED_DIR_NAME)
+# PROTECTED_MEDIA_URL = '//%s.%s.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, S3DIRECT_REGION, PROTECTED_DIR_NAME)
 
-MEDIA_ROOT = MEDIA_URL
+# MEDIA_ROOT = MEDIA_URL
 STATIC_URL = S3_URL + 'static/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
--- wm/settings.py
+++ wm/settings.py
@@ -171,7 +171,7 @@ MEDIA_ROOT  = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 
 
-from wm.aws.conf import *
+#from wm.aws.conf import *
 
 try:
     from .local_settings import *
