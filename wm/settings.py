e2fc067882e5c5e0eb863f2948b896b0eb649baf
Ingrid Anderson <ingrid@warriormedia.com>
2018-06-27 23:21:16 -0700

    Update settings.py

--- wm/settings.py
+++ wm/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '507fee1d-5885-45b6-96a5-6315cf0154fd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.kate-alm.herokuapp.com, kate-alm.herokuapp.com']
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
