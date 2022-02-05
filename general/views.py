b88cacd17bdfdb8b7cd36e41421f9a2f44ccfca1
eligilbert <eliasbgilbert@gmail.com>
2022-02-04 19:49:21 -0500

    harmony youth choir

--- general/views.py
+++ general/views.py
@@ -12,14 +12,11 @@ def general_main(request):
 
 def general_immersion(request):
     context = {}
-
     return(render(request, 'general/immersion.htm', context))
 
 def general_events(request):
     context = {}
-
     return(render(request, 'general/events.htm', context))
-
 
 def general_school_year(request):
     context = {}
@@ -32,3 +29,7 @@ def general_programs(request):
 def general_musicalsoulmates(request):
     context = {}
     return(render(request, 'general/musicalsoulmates.htm', context))
+
+def general_harmonyyouth(request):
+    context = {}
+    return(render(request, 'general/harmonyyouth.htm', context))
